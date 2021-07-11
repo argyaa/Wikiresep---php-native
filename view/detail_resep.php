ff63fe7f610d72619d2a94c2f50bb5d02aa8e8cc
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2021-07-11 14:51:56 +0700

    menambahkan function get data from database

--- view/detail_resep.php
+++ view/detail_resep.php
@@ -1,3 +1,12 @@
+<?php
+require_once('../service/koneksi.php');
+$db = dbConnect();
+session_start();
+if (isset($_POST['keluar'])) {
+    session_destroy();
+    header('location: ../index.php');
+}
+?>
 <!doctype html>
 <html lang="en">
 
@@ -20,73 +29,60 @@
 
 <body>
     <nav class="px-5 navbar navbar-expand-lg navbar-light bg-transparent mt-2 black-text-color">
-        <a class="navbar-brand logo black-text-color" href="/index.php"><span class="primary-text-color">WIKI</span>Resep</a>
+        <a class="navbar-brand logo black-text-color" href="../index.php"><span class="primary-text-color">WIKI</span>Resep</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active mr-4">
-                    <a class="nav-link button-text" href="../view/auth/login.php">Masuk <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link px-4 ly-2 primary-color rounded-pill button-text" href="../view/auth/regis.php">Daftar <span class="sr-only">(current)</span></a>
-                </li>
+        <?php
+        if (!isset($_SESSION['id'])) {
+        ?>
+            <div class="collapse navbar-collapse" id="n avbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active mr-4">
+                        <a class="nav-link button-text" href="view/auth/login.php">Masuk <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link px-4 ly-2 primary-color rounded-pill button-text" href="view/auth/regis.php">Daftar <span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+            </div>
+        <?php
+        } else {
+        ?>
+            <div class="collapse navbar-collapse" id="n avbarSupportedContent">
+                <form action="" method="post" class="ml-auto">
+                    <div class="dropdown">
+                        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Hai, <?php echo $_SESSION['username']; ?>
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="manage_user.php" name="kelola-user">Kelola User</a>
+                            <a class="dropdown-item" href="manage_resep.php" name="kelola-resep">Kelola Resep</a>
+                            <button class="dropdown-item" href="" name="keluar">Keluar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
 
-            </ul>
-        </div>
+        <?php
+        }
+        ?>
     </nav>
-
+    <?php $resep = getDataResepFromId($_GET['id'])[0]; ?>
     <div class="px-5 mt-5">
         <div class="row">
             <div class="col-12">
                 <div class="white container-regis black-text-color px-5 py-5">
-                    <div class="title-text text-center pt-5">Roti Sobek Daging Asap</div>
-                    <div class="subtitle-text black-text-color px-5 mt-3 text-center">Cocok dipadukan dengan secangkir kopi atau teh hangat. Yuk, cari tahu resep roti sobek smoked beef yang membuat momen bersama keluarga semakin istimewa!</div>
-                    <div class="body-text text-center secondary-text-color mt-3">oleh Dandi</div>
+                    <div class="title-text text-center pt-5"><?php echo $resep['judul']; ?></div>
+                    <div class="subtitle-text black-text-color px-5 mt-3 text-center"><?php echo $resep['deskripsi']; ?></div>
+                    <div class="body-text text-center secondary-text-color mt-3">Oleh <?php echo $resep['username']; ?></div>
                     <div class="d-flex justify-content-center">
-                        <img class="mt-5 hero-img" src="https://www.masakapahariini.com/wp-content/uploads/2020/10/roti-sobek-smoked-beef-780x440.jpg" alt="">
+                        <img class="mt-5 hero-img" src="<?php echo $resep['gambar']; ?>" alt="">
                     </div>
 
                     <div class="body-text black-text-color text-left mt-5 px-5">
-                        Jika kita biasanya hanya memadukan roti dengan mentega lalu ditaburi gula, kini ada satu cara yang lebih seru untuk teman kamu bersantai di rumah. Resep kali ini gurih, harum, dan cocok disantap setiap saat. Roti sobek smoked beef bisa menjadi pilihan saat menghabiskan waktu bersama teman dan keluarga.
-
-                        Konsep dari roti sobek smoked beef sendiri mirip dengan jenis lainnya. Namun, alih-alih menggunakan isian manis seperti cokelat, keju, ataupun selai, resep ini memadukan kelembutan roti dengan topping gurih berupa smoked beef, mayones, serta tak lupa Saus Sambal Jawara Hot yang nendang di mulut dan bikin ingin nambah terus. Cara membuatnya pun mudah, cukup dengan bahan-bahan yang tersedia di dapur saja.
-
-                        Tersaji hangat dari bara oven, padukan roti sobek smoked beef dengan secangkir teh atau kopi. Maka, lengkap sudah momen istimewa bersama orang-orang tersayang! Penasaran dengan resep roti lain yang tak kalah lezatnya dengan yang satu ini? Cobain resep unik lainnya seperti roti jala kuah kari, roti canai, ataupun Korean Garlic Bread yang tak kalah menggoda selera!
-
-                        Tunggu apa lagi? Yuk, kita berangkat ke dapur!<br><br>
-
-
-                        bahan-bahan<br>
-                        <br>
-                        2 kuning telur ayam<br>
-                        50g mentega tawar<br>
-                        150ml susu cair hangat<br>
-                        400g tepung terigu protein tinggi<br>
-                        3sdm susu bubuk<br>
-                        1sdt garam<br>
-                        100g gula pasir<br>
-                        1.5sdt ragi instan<br>
-                        <br>
-                        olesan<br><br>
-                        1 kuning telur ayam, campur dengan 2 sdm susu cair<br>
-                        1sdm peterseli<br>
-
-                        isi<br><br>
-                        12 lembar daging Asap<br>
-                        4 sdm jawara saus sambal Hot<br>
-                        2 sdm hellman's Real Mayonnaise<br>
-                        50g keju cheddar, parut<br><br>
-
-
-                        Cara membuat<br><br>
-                        1. Olesi loyang ukuran 15x10x4 cm dengan mentega hingga rata. Panaskan oven pada suhu 200° C. Sisihkan. Campur bahan kering dengan telur, aduk rata. Tuang susu cair sedikit demi sedikit, sambil diuleni hingga tercampur rata. Tambahkan mentega, uleni kembali hingga kalis.<br>
-                        2. Letakkan adonan di dalam mangkuk, tutup dengan plastik. Sisihkan hingga mengembang 2 kali (± 30 menit).<br>
-                        3. Kempiskan adonan, gilas menggunakan rolling pin. Susun melebar smoked beef ke atasnya. Olesi dengan mayones dan Saus Sambal Jawara Hot. Tambahkan keju cheddar parut. Gulung roti. Potong-potong setebal 3 cm.<br>
-                        4. Siapkan loyang kotak yang sudah diolesi mentega secara merata. Tata potongan roti ke atasnya. Diamkan hingga mengembang (± 15 menit).<br>
-                        5. Olesi permukaan roti dengan kuning telur secara merata. Taburi dengan peterseli. Panggang di dalam oven panas hingga matang (± 20 menit), keluarkan. Olesi permukaannya dengan mentega. Sajikan.<br>
+                        <?php echo $resep['konten']; ?>
                     </div>
                 </div>
             </div>
